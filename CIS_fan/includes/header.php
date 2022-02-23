791750b0d97cd1c6d26b9e1e603aac70d66e02e0
LudwigBoudeweelOfficiel <99881598+LudwigBoudeweelOfficiel@users.noreply.github.com>
2022-02-23 22:24:16 +0100

    Add files via upload

---
+++ CIS_fan/includes/header.php
@@ -0,0 +1,4 @@
+<section class="header">
+	<img src="images/ic_feu.png"/>
+	<h2>Fan CIS <?php include('valeurs/namecis.php');?></h2>
+</section>
---
+++ CIS_fan/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>CIS Fan</title>
+		<link rel="stylesheet" href="style.css"/>
+	</head>
+	
+	<body>
+		<!-- menu -->
+		<?php include('includes/header.php');?>
+	</body>
+</html>
